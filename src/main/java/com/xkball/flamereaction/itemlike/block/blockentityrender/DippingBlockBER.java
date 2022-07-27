7a73e230baaedf1a9993de93702ea69a0b9a870a
xkball <3324786417@qq.com>
2022-07-28 00:16:52 +0800

    应该能进服务器,更新浸洗盆和其他

--- src/main/java/com/xkball/flamereaction/itemlike/block/blockentityrender/DippingBlockBER.java
+++ src/main/java/com/xkball/flamereaction/itemlike/block/blockentityrender/DippingBlockBER.java
@@ -2,10 +2,13 @@ package com.xkball.flamereaction.itemlike.block.blockentityrender;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Vector3f;
+import com.xkball.flamereaction.FlameReaction;
 import com.xkball.flamereaction.eventhandler.register.FluidRegister;
 import com.xkball.flamereaction.itemlike.block.blockentity.DippingBlockEntity;
 import com.xkball.flamereaction.render.FluidRenderFromMantle;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.core.Direction;
@@ -21,7 +24,17 @@ public class DippingBlockBER implements BlockEntityRenderer<DippingBlockEntity> 
         var texture = FluidRenderFromMantle.getBlockSprite(FluidRegister.FLOWING_TEXTURE);
         var color = FluidRegister.IMPURE_ALCOHOL_COLOR.getRGB();
         var from = Vector3f.ZERO.copy();
+        var item = entity.getItem();
         var to = new Vector3f(1.0F,3/16F,1.0F);
         FluidRenderFromMantle.renderFluid(poseStack,buffer,texture, Direction.UP,from,to,color,1);
+    
+        poseStack.pushPose();
+        poseStack.translate(0.5,0.374,0.5);
+        
+        if(!item.is(FlameReaction.PLIERS)){
+            poseStack.mulPose(Vector3f.XP.rotationDegrees(90.0F));
+        }
+        Minecraft.getInstance().getItemRenderer().renderStatic(item, ItemTransforms.TransformType.FIXED,combinedLightIn,combinedOverlayIn,poseStack,multiBufferSource,1);
+        poseStack.popPose();
     }
 }
